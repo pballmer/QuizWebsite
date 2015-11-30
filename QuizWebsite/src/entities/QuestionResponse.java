8de3e65ab7cc8b53e65d477468936482b95b3087
StephenAman <stephenaman3@gmail.com>
2015-11-30 13:34:12 -0800

    Post Thanksgiving commit
    Nada

--- QuizWebsite/src/entities/QuestionResponse.java
+++ QuizWebsite/src/entities/QuestionResponse.java
@@ -8,7 +8,5 @@ public class QuestionResponse extends QuestionAbstract{
 	public QuestionResponse(int questionID, int quizID, String question, Set<ArrayList<String>> answers){
 		super(questionID, quizID, question, answers);
 		this.type = questionTypes[3];
-
 	}
-
 }
