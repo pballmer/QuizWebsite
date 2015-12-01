5d4bd7eff2e8c17bcfd6a0b20060cb7569840bd6
StephenAman <stephenaman3@gmail.com>
2015-11-30 22:34:22 -0800

    Fixing Notifications in Entities

---
+++ QuizWebsite/src/entities/NotificationAbstract.java
@@ -0,0 +1,8 @@
+
+public abstract class NotificationAbstract {
+	//0 is friend request, 1 is challenge, 2 is note
+	public static final int[] NotificationTypes = {0, 1, 2};
+	public int notificationID;
+	
+	
+}
