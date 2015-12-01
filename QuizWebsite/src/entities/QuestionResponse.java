faf9826ce7b418bb908e4178cefe79f7b837f444
StephenAman <stephenaman3@gmail.com>
2015-11-30 22:31:46 -0800

    QuestionResponse Added

---
+++ QuizWebsite/src/entities/QuestionResponse.java
@@ -0,0 +1,16 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import entities.QuestionAbstract;
+
+public class QuestionResponse extends QuestionAbstract{
+	
+	public QuestionResponse(int questionID, int quizID, String question, ArrayList<String> answers){
+		super(questionID, quizID, question, answers);
+		this.type = QUESTION_RESPONSE;
+
+	}
+
+}
