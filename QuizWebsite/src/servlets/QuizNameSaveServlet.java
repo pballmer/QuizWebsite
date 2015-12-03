b7b54351db662e4c935bf3dcdd8e6a609b2f16a0
ckincaid17 <colin.kincaid.school@gmail.com>
2015-12-02 20:51:51 -0800

    why didn't these commit with my last commit?

---
+++ QuizWebsite/src/servlets/QuizNameSaveServlet.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class QuizNameSaveServlet
+ */
+@WebServlet("/QuizNameSaveServlet")
+public class QuizNameSaveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuizNameSaveServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String quizName = request.getParameter("name");
+		HttpSession session = request.getSession();
+        session.setAttribute("quizName", quizName);
+	}
+
+}
